a297b4438ed0611cd887f7f6a01ce501d76ea973
Akshat7666 <71199447+Akshat7666@users.noreply.github.com>
2020-10-11 21:56:11 +0530

    Add files via upload

---
+++ goodbye.js
@@ -0,0 +1,10 @@
+(function (window) {
+var speakword ={};
+var greeting = "Goodbye ";
+speakword.saygoodbye = function () {
+	console.log(greeting + speakword.names);
+}
+
+window.speakword = speakword;
+
+})(window);
